62253101474e7d1df986293ab551bd018d276ad7
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-12-22 11:40:55 +0700

    Membuat Data Dao Barang

--- app/src/main/java/com/example/ucp2/data/dao/BarangDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/BarangDao.kt
@@ -1,4 +1,34 @@
+
 package com.example.ucp2.data.dao
 
-interface BarangDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import com.example.ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BarangDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBarang(barang: Barang)
+
+    @Delete
+    suspend fun deleteBarang(barang:Barang)
+
+    @Update
+    suspend fun updateBarang(barang: Barang)
+
+    @Transaction
+    @Query("SELECT * FROM barang")
+    fun  getAllBarang():Flow<List<Barang>>
+
+    @Transaction
+    @Query("SELECT * FROM barang WHERE id = :id")
+    fun getBarangDetail(id : String) : Flow<Barang>
+
+
 }
