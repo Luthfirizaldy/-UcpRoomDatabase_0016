942e25e4af2def2a1cba8dbd6a48ec1b7e812bc6
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-12-22 09:23:22 +0700

    Membuat Entity Barang

--- app/src/main/java/com/example/ucp2/data/entity/Barang.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Barang.kt
@@ -1,3 +1,27 @@
 package com.example.ucp2.data.entity
 
-data class Barang()
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity  (tableName = "barang",
+    foreignKeys = [
+        ForeignKey(
+            entity = Suplier::class,
+            parentColumns = ["id"],
+            childColumns = ["id_suplier"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["id_suplier"])]
+)
+data class Barang(
+    @PrimaryKey
+    val id : String,
+    val nama : String,
+    val deskripsi : String,
+    val harga : String,
+    val stok : String,
+    val id_suplier : String
+)
