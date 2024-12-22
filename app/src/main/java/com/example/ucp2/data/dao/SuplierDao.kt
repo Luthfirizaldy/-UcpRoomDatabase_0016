0abb4c068a101ddef77da9950999e488572546bf
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-12-22 09:47:35 +0700

    Membuat Data Dao Suplier

--- app/src/main/java/com/example/ucp2/data/dao/SuplierDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/SuplierDao.kt
@@ -1,4 +1,26 @@
 package com.example.ucp2.data.dao
 
-interface SuplierDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SuplierDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSuplier(suplier: Suplier)
+
+    @Query("SELECT * FROM suplier")
+    fun getAllSuplier():Flow<List<Suplier>>
+
+    @Query("SELECT * FROM suplier WHERE id = :id")
+    fun getSuplierById(id : String) : Flow<Suplier>
+
+    @Transaction
+    @Query("SELECT * FROM suplier WHERE nama = :nama")
+    fun  geSuplierByNama(nama : String) : Flow<Suplier>
 }
